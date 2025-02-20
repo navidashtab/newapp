cff854dee9825c21675b617fe3b8708d9c4842a4
navidashtab <35516295+navidashtab@users.noreply.github.com>
2025-02-20 10:58:18 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
          const heading = document.querySelector('h1');
          heading.addEventListener('click', () => {
-             alert('سلام! روی عنوان کلیک کردی!');
+             alert('سلام');
          });
      });
